1651b36f3374d3bf1f33647587c1125c7d88a6f8
Abdullah Al Akash <aabdullahalakash@gmail.com>
2022-02-28 23:12:06 +0600

    Handle When No Result Found

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-console.log("Hello Meal DB");
-
+// Search Food:
 const searchFood = () => {
         const searchField = document.getElementById('search-field');
         const searchValue = searchField.value;
@@ -13,24 +12,64 @@ const searchFood = () => {
 
 // Display Meal:
 const displayMeals = meals => {
-        meals.forEach(meal => {
-                console.log(meal)
-                const div = document.createElement('div');
-                div.classList.add('col');
-                div.innerHTML = `
-                        <div class="card">
-                                <div class="p-5">
-                                        <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+        const mealContainer = document.getElementById('meal-container');
+        const errorContainer = document.getElementById('error-message')
+
+        // Clear Value:
+        mealContainer.textContent = '';
+        errorContainer.textContent = '';
+        console.log(meals)
+        if (meals === null) {
+                const p = document.createElement('p');
+                p.classList.add('text-center');
+                p.classList.add('text-danger');
+                p.classList.add('fw-bold');
+                p.innerText = "Sorry! There is no result found";
+                errorContainer.appendChild(p);
+        }
+        else {
+                meals.forEach(meal => {
+                        const div = document.createElement('div');
+                        div.classList.add('col');
+                        div.innerHTML = `
+                                <div onclick="loadMealDetail(${meal.idMeal})" class="card">
+                                        <div class="p-5">
+                                                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                                        </div>
+                                        <div class="card-body">
+                                                <h5 class="card-title">Card title</h5>
+                                                <p class="card-text">This is a longer card with supporting text
+                                                        below as a natural lead-in to additional content. This
+                                                        content is a little bit longer.</p>
+                                        </div>
                                 </div>
-                                <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <p class="card-text">This is a longer card with supporting text
-                                                below as a natural lead-in to additional content. This
-                                                content is a little bit longer.</p>
-                                </div>
+                        `;
+                        mealContainer.appendChild(div);
+                })
+        }
+}
+
+// Display Meal Detail:
+const loadMealDetail = meal => {
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meal}`)
+                .then(res => res.json())
+                .then(data => displayMealDetail(data.meals[0]))
+}
+
+const displayMealDetail = (meal) => {
+        console.log(meal)
+        const mealDetailContainer = document.getElementById('meal-detail');
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+                <img src="${meal.strMealThumb}" class="card-img-top p-4" alt="...">
+                <div class="card-body">
+                        <h5 class="card-title">${meal.strMeal}</h5>
+                        <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
+                        <div class="d-grid gap-2">
+                                <a target="_blank" href="${meal.strYoutube}" class="btn btn-success">See More</a>
                         </div>
-                `;
-                const mealContainer = document.getElementById('meal-conatiner');
-                mealContainer.appendChild(div);
-        })
+                </div>
+        `;
+        mealDetailContainer.appendChild(div);
 }
